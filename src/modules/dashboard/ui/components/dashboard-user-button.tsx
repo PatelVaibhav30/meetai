5087241cfeed4c9f7cdfc1e3d122efa0804b5085
Vaibhav <patelvaibhav30301@gmail.com>
2025-07-05 23:29:55 +0530

    feat: implement dashboard layout with navbar and command dialog, enhance user button with mobile support

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,13 +1,17 @@
 import { authClient } from "@/lib/auth-client";
 
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Drawer, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { GeneratedAvatar } from "@/components/generated-avatar";
-import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from "lucide-react";
+import { ChevronDownIcon, CreditCardIcon, LogOut, LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 
 export const DashboardUserButton = () => {
     const router = useRouter();
+    const isMobile = useIsMobile();
     const { data, isPending } = authClient.useSession();
 
     const onLogout = () => {
@@ -22,6 +26,49 @@ export const DashboardUserButton = () => {
     if (isPending || !data) {
         return null
     }
+
+    if (isMobile) {
+        return (
+            <Drawer>
+                <DrawerTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+                    {data.user.image ? (
+                        <Avatar>
+                            <AvatarImage src={data.user.image} alt={data.user.name} />
+                        </Avatar>
+                    ) : (
+                        <GeneratedAvatar
+                            seed={data.user.name}
+                            variant="initials"
+                            className="size-9 mr-3"
+                        />
+                    )}
+                    <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
+                        <p className="text-sm truncate w-full">{data.user.name}</p>
+                        <p className="text-xs truncate w-full">{data.user.email}</p>
+                    </div>
+                    <ChevronDownIcon className="size-4 shrink-0" />
+                </DrawerTrigger>
+                <DrawerContent className="w-72">
+                    <DrawerHeader>
+                        <DrawerTitle>{data.user.name}</DrawerTitle>
+                        <DrawerDescription>{data.user.email}</DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Button variant="outline" onClick={() => { }}>
+                            Billing
+                            <CreditCardIcon className="size-4 text-black" />
+                        </Button>
+
+                        <Button variant="outline" onClick={onLogout}>
+                            Logout
+                            <LogOutIcon className="size-4 text-black" />
+                        </Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
+        )
+    }
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
@@ -54,9 +101,9 @@ export const DashboardUserButton = () => {
                     Billing
                     <CreditCardIcon className="size-4" />
                 </DropdownMenuItem>
-                <DropdownMenuItem 
-                onClick={onLogout}
-                className="cursor-pointer flex items-center justify-between">
+                <DropdownMenuItem
+                    onClick={onLogout}
+                    className="cursor-pointer flex items-center justify-between">
                     Logout
                     <LogOutIcon className="size-4" />
                 </DropdownMenuItem>
